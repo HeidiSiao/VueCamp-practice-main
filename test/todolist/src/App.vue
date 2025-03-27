108cdb849da46594b4f116fa0234baabaab2cf2b
HeidiSiao <heidi.siao@gmail.com>
2025-03-27 23:40:05 +0800

    add: todo-addItem

---
+++ test/todolist/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TestA from './components/TestA.vue'
+</script>
+
+<template>
+  <h2>這是app.vue的地盤</h2>
+  <TestA />
+</template>
+
+<style scoped>
+</style>
---
+++ test/todolist/src/components/TestA.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+const text = ref('這裡使用了ref');
+const addItems = () => {
+  todos.value.push({
+    id: new Date().getTime(),
+    text: text.value
+  })
+  text.value="";
+};
+
+const todos =ref([
+  {
+    id: 123,
+    text: '我是文字'
+  }
+]) ;
+
+</script>
+
+<template>
+  <input type="text" v-model="text">
+  <button type="button" @click="addItems">新增待辦</button>
+  <div v-for="todo in todos" :key="todo.id">
+{{ todo.text }}
+  </div>
+</template>
