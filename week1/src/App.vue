3b9d6d2ea3baa68462fc8b10510f4b19b3d4d2df
HeidiSiao <heidi.siao@gmail.com>
2025-05-11 03:26:01 +0800

    week02 review practice

--- week1/src/App.vue
+++ week1/src/App.vue
@@ -1,70 +1,65 @@
 <script setup>
 import { ref } from "vue";
+import VueComponent from "./components/VueComponent.vue";
 
 // 把資料抽出來
 // 由資料驅動畫面 響應式
 const items = ref([
-{
+  {
     id: 1,
-    name: '購買雜貨',
-    dueDate: '2024-07-30',
-    completed: true
+    name: "購買雜貨",
+    dueDate: "2024-07-30",
+    completed: true,
   },
   {
     id: 2,
-    name: '完成報告',
-    dueDate: '2024-08-01',
-    completed: false
+    name: "完成報告",
+    dueDate: "2024-08-01",
+    completed: false,
   },
   {
     id: 3,
-    name: '清理房間',
-    dueDate: '2024-07-28',
-    completed: true
+    name: "清理房間",
+    dueDate: "2024-07-28",
+    completed: true,
   },
   {
     id: 4,
-    name: '計劃假期',
-    dueDate: '2024-08-05',
-    completed: false
+    name: "計劃假期",
+    dueDate: "2024-08-05",
+    completed: false,
   },
   {
     id: 5,
-    name: '處理稅務',
-    dueDate: '2024-08-10',
-    completed: false
-  }
+    name: "處理稅務",
+    dueDate: "2024-08-10",
+    completed: false,
+  },
 ]);
-
-console.log(items.value)
-
 </script>
-
 
 <template>
   <h2>Week-01</h2>
   <table>
-  <thead>
-    <tr>
-      <th>代辦事項名稱</th>
-      <th>到期日</th>
-      <th>完成狀況</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="item of items" :key="item.id">
-      <td>{{ item.name }}</td>
-      <td>{{ item.dueDate }}</td>
-      <td>{{ item.completed ? '是':'否' }}</td>
-      <!-- 三元運算 -->
-    </tr>
-    
-  </tbody>
-</table>
-  <VueComponent></VueComponent>
- </template>
+    <thead>
+      <tr>
+        <th>代辦事項名稱</th>
+        <th>到期日</th>
+        <th>完成狀況</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item of items" :key="item.id">
+        <td>{{ item.name }}</td>
+        <td>{{ item.dueDate }}</td>
+        <td>{{ item.completed ? "是" : "否" }}</td>
+        <!-- 三元運算 -->
+      </tr>
+    </tbody>
+  </table>
+  <hr />
+  <h2>Week-02</h2>
+  <VueComponent />
+</template>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- week1/src/components/VueComponent.vue
+++ week1/src/components/VueComponent.vue
@@ -1,9 +1,111 @@
 <template>
-
+  <div class="flex-col">
+    <label for="name">商品名稱</label>
+    <input class="space-s" type="text" v-model="newName" id="name" />
+    <label for="account">商品數量</label>
+    <input class="space-s" type="number" v-model="newNumber" id="account" />
+    <button class="space-s" type="button" @click="addProduct">增加</button>
+  </div>
+  <div class="space-s">
+    {{ items }}
+  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>標題</th>
+        <th>數量</th>
+        <th>調整數量</th>
+        <th>刪除</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in items" :key="item.id">
+        <td>{{ item.name }}</td>
+        <td>{{ item.account }}</td>
+        <td><input type="number" v-model="item.account" /></td>
+        <td>
+          <button type="button" @click="deleteItem(item.id)">刪除品項</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
+const newName = ref("");
+const newNumber = ref(0);
+
+const items = ref([
+  {
+    id: 1,
+    name: "蜂蜜鬆餅",
+    account: 42,
+  },
+  {
+    id: 2,
+    name: "草莓大福",
+    account: 7,
+  },
+  {
+    id: 3,
+    name: "珍珠奶茶",
+    account: 89,
+  },
+  {
+    id: 4,
+    name: "芋泥球",
+    account: 15,
+  },
+  {
+    id: 5,
+    name: "抹茶蛋糕",
+    account: 63,
+  },
+]);
+
+const addProduct = () => {
+  items.value.push({
+    id: new Date().getTime(),
+    name: newName.value,
+    account: newNumber.value,
+  });
+  newName.value = "";
+  newNumber.value = 0;
+};
+
+const deleteItem = (targetId) => {
+  // 防呆:避免在找不到對應項目時誤刪錯誤資料index是-1的value
+  //   if (index !== -1) {
+  //   items.value.splice(index, 1);
+  // }
+  items.value = items.value.filter((item) => item.id !== targetId);
+};
 </script>
 
 <style scoped>
+.space {
+  margin: 50px;
+}
+
+.space-s {
+  margin: 8px;
+}
+
+table,
+td {
+  border-collapse: separate;
+  border-spacing: 20px 10px;
+  border: 1px solid tan;
+  border-radius: 5px;
+  padding: 4px;
+  text-align: center;
+}
+
+.flex-col {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+}
 </style>
